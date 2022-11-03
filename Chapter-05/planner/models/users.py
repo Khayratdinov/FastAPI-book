bd6e3a6abd602811977d1691740a973c6ddcbd80
Nurpolat <khayratdinov.np@gmail.com>
2022-11-03 14:45:58 +0500

    chore: 🎨 Structuring in FastAPI applications

---
+++ Chapter-05/planner/models/users.py
@@ -0,0 +1,21 @@
+from pydantic import BaseModel, EmailStr
+
+
+class User(BaseModel):
+    email: EmailStr
+    password: str
+
+    class Config:
+        schema_extra = {
+            "example": {
+                "email": "fastapi@packt.com",
+                "password": "strong!!!",
+            }
+        }
+
+
+class UserSignIn(BaseModel):
+    email: EmailStr
+    password: str
+
+    schema_extra = {"example": {"email": "fastapi@packt.com", "password": "strong!!!"}}
