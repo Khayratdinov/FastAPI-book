7f17b933543b198d20cef14cb758b45188b9cb73
khayratdinov <khayratdinov.np@gmail.com>
2022-11-20 11:27:46 +0500

    chore: 🏁 finish Chapter-05 Structuring FastAPI Applications

--- Chapter-05/planner/models/users.py
+++ Chapter-05/planner/models/users.py
@@ -1,9 +1,12 @@
 from pydantic import BaseModel, EmailStr
+from typing import Optional, List
+from models.events import Event
 
 
 class User(BaseModel):
     email: EmailStr
     password: str
+    events: Optional[List[Event]]
 
     class Config:
         schema_extra = {
