943e6b1c657f514d8f7cb27f8b9d5440dac3ffab
khayratdinov <khayratdinov.np@gmail.com>
2022-11-24 00:38:18 +0500

    chore: CRUD operations in SQL database using SQLModel

--- Chapter-06/planner/main.py
+++ Chapter-06/planner/main.py
@@ -1,5 +1,8 @@
 from fastapi import FastAPI
 from fastapi.responses import RedirectResponse
+from fastapi.staticfiles import StaticFiles
+
+from database.connection import conn
 
 from routes.users import user_router
 from routes.events import event_router
@@ -10,14 +13,20 @@ app = FastAPI()
 
 # Register routes
 
-app.include_router(user_router, prefix="/user")
+app.include_router(user_router,  prefix="/user")
 app.include_router(event_router, prefix="/event")
+
+app.mount("/images", StaticFiles(directory="images"), name="images")
+
+
+@app.on_event("startup")
+def on_startup():
+    conn()
 
 
 @app.get("/")
 async def home():
     return RedirectResponse(url="/event/")
 
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     uvicorn.run("main:app", host="0.0.0.0", port=8080, reload=True)
